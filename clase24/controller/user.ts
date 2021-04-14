8914f7203ab704970384e7cc1bdff95bae737fe7
Franco Toniolo <francotoniolo@MMBA-B294.local>
2021-04-14 08:18:43 -0300

    Clase 24 - Agregar nuevos usuarios

--- clase24/controller/user.ts
+++ clase24/controller/user.ts
@@ -2,13 +2,12 @@
 export const login = (req,res) => {
     const {user, password } = req.body;
 
-    if (user === 'ftoniolo' && password === '123') {
-        req.session.user= "Franco";
+    if ((user === 'ftoniolo' && password === '123') || (user === 'gonzalo' && password === '123') ) {
+        req.session.user= user;
         req.session.login= true;
         res.redirect('/chat');
 
     } else {
-        console.log(req.session.user);
         res.render('user/login',{err: "Credenciales incorrectas"});
     }
 }
